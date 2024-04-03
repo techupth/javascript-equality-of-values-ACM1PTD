d23da777f080c37236cfad492859cbddf8fad23a
Nonthawath Patchakapati <npc2540@hotmail.com>
2024-04-03 19:18:09 +0700

    add answers

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,12 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; // true
+console.log(0 == false)
+null == undefined; // true
+console.log(null == undefined)
+10 == "10"; // true
+console.log(10 == "10")
+[10] == 10; // true
+console.log([10] == 10)
+[1,2] == "1,2"; // true
+console.log([1,2] == "1,2")
